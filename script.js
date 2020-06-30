cc8ca821da1761b076ef3feb9ec71540fa28190e
d4c-tti <66168738+d4c-tti@users.noreply.github.com>
2020-06-30 15:21:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 $(function() {
 
-  $('#modal-show-login').click(function(){
+  $('.modal-show-login').click(function(){
     $('#modal-wrapper').fadeIn();
   });
+
+  $('.modal-show-signin').click(function(){
+    $('#modal-wrapper-sign').fadeIn();
+  });
+
+  $('.modal-close').click(function(){
+    $('#modal-wrapper').fadeOut();
+    $('#modal-wrapper-sign').fadeOut();
+  });
+
+  $('#page-top').click(function(){
+    $('html,body').animate({
+      'scrollTop':0
+    }, 'slow');
+  });
+
+  $('.btn-lesson').click(function(){
+    var id = $(this).attr('href');
+    var positon = $(id).offset().top;
+    $('html,body').animate({
+      'scrollTop':positon
+    }, 'slow');
+  });
+
+  $('.sp-menu').click(function(){
+    if($('#sp-btn').hasClass('open')){
+      $('#sp-btn').removeClass('open');
+      $('#sp-btn').slideUp();
+    } else {
+      $('#sp-btn').addClass('open');
+      $('#sp-btn').slideDown();
+    }
+
+  });
+
+
+
 });
